ed53f4f09107be75d3f3ad4f98367ca20ed6b66f
Simon Biggs <simon.biggs@radiotherapy.ai>
2023-03-15 11:31:27 +1100

    prep for merging

--- evals/elsuite/basic/list_match.py
+++ evals/elsuite/basic/list_match.py
@@ -40,12 +40,6 @@ class ListMatch(evals.Eval):
             recall = 1.0 * num_same / len(tools_ground_truth)
             f1 = (2 * precision * recall) / (precision + recall)
 
-        # TODO: Remove this debugging print before changing PR to ready for review
-        question = sample["input"][-1]["content"]
-        print(
-            f"\n{question}:\nPredicted: {tools_predicted}\nGround Truth: {tools_ground_truth}\nRecall: {recall}\n"
-        )
-
         evals.record.record_metrics(
             accuracy=float(tools_predicted == tools_ground_truth),
             f1_score=f1,
