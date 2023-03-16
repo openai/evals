185cf571245b9b6fe7572edb9e6c3ed36d617bd7
Kiarash Jamali <kjamali@mrc-lmb.cam.ac.uk>
2023-03-16 21:10:45 +0000

    Create matrix_multiplication.py

---
+++ evals/elsuite/matrix_multiplication.py
@@ -0,0 +1,55 @@
+import random
+import textwrap
+
+import evals
+import evals.metrics
+
+class MatrixMultiplication(evals.Eval):
+    def __init__(self, train_jsonl, test_jsonl, train_samples_per_prompt=2, **kwargs):
+        super().__init__(**kwargs)
+        self.train_jsonl = train_jsonl
+        self.test_jsonl = test_jsonl
+        self.train_samples_per_prompt = train_samples_per_prompt
+
+    def run(self, recorder):
+        """
+        Called by the `oaieval` CLI to run the eval. The `eval_all_samples` method calls `eval_sample`.
+        """
+        self.train_samples = evals.get_jsonl(self.train_jsonl)
+        test_samples = evals.get_jsonl(self.test_jsonl)
+        self.eval_all_samples(recorder, test_samples)
+
+        # Record overall metrics
+        return {
+            "accuracy": evals.metrics.get_accuracy(recorder.get_events("match")),
+        }
+ 
+    def eval_sample(self, test_sample, rng: random.Random):
+        """
+        Called by the `eval_all_samples` method to evaluate a single sample.
+
+        ARGS
+        ====
+        `test_sample`: a line from the JSONL test file
+        `rng`: should be used for any randomness that is needed during evaluation
+
+        This method does the following:
+        1. Generate a prompt that contains the task statement, a few examples, and the test question.
+        2. Check if the model generates the correct answer.
+        """
+        stuffing = rng.sample(self.train_samples, self.train_samples_per_prompt)
+
+        prompt = [
+            {"role": "system", "content": "Solve the following problem, where the @ sign signifies matrix multiplication"},
+        ]
+
+        for i, sample in enumerate(stuffing + [test_sample]):
+            if i < len(stuffing):
+                prompt += [
+                    {"role": "system", "content": sample["problem"], "name": "example_user"},
+                    {"role": "system", "content": sample["answer"], "name": "example_assistant"},
+                ]
+            else:
+                prompt += [{"role": "user", "content": sample["problem"]}]
+
+        evals.check_sampled_text(self.model_spec, prompt, expected=sample["answer"])
