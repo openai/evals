856229c2da105ced4ecfac732f5d6c48fe4f6c5a
Shane Gu <shane@openai.com>
2023-03-23 09:14:27 -0700

    fix

--- evals/elsuite/modelgraded/classify.py
+++ evals/elsuite/modelgraded/classify.py
@@ -263,8 +263,8 @@ class ModelBasedClassify(evals.Eval):
                             completion += format_necessary(
                                 completion_i_template,
                                 i=i + 1,
-                                i_abc=string.ascii_lowercase[i],
-                                i_ABC=string.ascii_uppercase[i],
+                                i_abc=string.ascii_lowercase[i % 26],
+                                i_ABC=string.ascii_uppercase[i % 26],
                                 output=completion_i,
                                 n=self.multicomp_n,
                             )
