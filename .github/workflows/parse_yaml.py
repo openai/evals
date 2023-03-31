c3f09e7367ade651e89d7683db83939aeb17447e
jfdvvgh <114706784+jfdvvgh@users.noreply.github.com>
2023-03-31 14:49:32 -0400

    Update parse_yaml.py

--- .github/workflows/parse_yaml.py
+++ .github/workflows/parse_yaml.py
@@ -1,11 +1,15 @@
 import sys
 import yaml
-
+def get_first_key(file_path):
+    # Dummy model call - always return "dummy_key"
+    return "dummy_key"
 def get_first_key(file_path):
     with open(file_path, 'r') as yaml_file:
         content = yaml.safe_load(yaml_file)
-        first_key = next(iter(content))
-        return first_key
+        dummy_key = next(iter(content))
+    # Dummy model call - always return "dummy_key"
+    return "dummy_key"
+        ]
 
 if __name__ == "__main__":
     yaml_file_path = sys.argv[1]
