fd260ae50efeb68473ede78d5ed8e93911b82ab2
Alvin Wang <jwang47@users.noreply.github.com>
2023-05-02 15:15:11 -0700

    MMLU eval

---
+++ evals/elsuite/academic/mmlu.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+import evals
+from evals.api import CompletionFn
+from evals.elsuite import utils
+from evals.record import RecorderBase
+from datasets import load_dataset
+
+
+class MMLU(evals.Eval):
+    def __init__(
+        self,
+        completion_fns: list[CompletionFn],
+        samples_jsonl: str,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(completion_fns, *args, **kwargs)
+        assert len(completion_fns) == 1, "MMLU only supports one completion fn"
+
+    def eval_sample(self, test_sample, rng):
+        del rng
+        prompt, correct_answers = test_sample["input"], test_sample["ideal"]
+        if not isinstance(correct_answers, list):
+            correct_answers = [correct_answers]
+
+        result = self.completion_fn(
+            prompt=prompt,
+            temperature=0.0,
+            max_tokens=1
+        )
+        sampled = result.get_completions()[0]
+
+        matches = [utils.fuzzy_match(sampled, correct_answer) for correct_answer in correct_answers]
+
+        evals.record.record_match(
+            True in matches,
+            expected=correct_answers,
+            picked=[sampled for i in range(len(correct_answers)) if matches[i]],
+        )
+        evals.record.record_metrics(
+            accuracy=float(True in matches),
+            f1_score=utils.f1_score(sampled, correct_answers),
+        )
+
+    def run(self, recorder: RecorderBase):
+        configs = ['abstract_algebra', 'anatomy', 'astronomy', 'business_ethics', 'clinical_knowledge', 'college_biology', 'college_chemistry', 'college_computer_science', 'college_mathematics', 'college_medicine', 'college_physics', 'computer_security', 'conceptual_physics', 'econometrics', 'electrical_engineering', 'elementary_mathematics', 'formal_logic', 'global_facts', 'high_school_biology', 'high_school_chemistry', 'high_school_computer_science', 'high_school_european_history', 'high_school_geography', 'high_school_government_and_politics', 'high_school_macroeconomics', 'high_school_mathematics', 'high_school_microeconomics', 'high_school_physics', 'high_school_psychology', 'high_school_statistics', 'high_school_us_history', 'high_school_world_history', 'human_aging', 'human_sexuality', 'international_law', 'jurisprudence', 'logical_fallacies', 'machine_learning', 'management', 'marketing', 'medical_genetics', 'miscellaneous', 'moral_disputes', 'moral_scenarios', 'nutrition', 'philosophy', 'prehistory', 'professional_accounting', 'professional_law', 'professional_medicine', 'professional_psychology', 'public_relations', 'security_studies', 'sociology', 'us_foreign_policy', 'virology', 'world_religions']
+        dataset = load_dataset("hendrycks_test", configs[0])
+
+        samples = self.get_samples()
+        self.eval_all_samples(recorder, samples)
+
+        return {
+            "accuracy": np.mean(recorder.get_scores("accuracy")),
+            "f1_score": np.mean(recorder.get_scores("f1_score")),
+        }
