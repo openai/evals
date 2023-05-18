06f967176d1d7dab7e403d78bcb65eb965962002
Andrew Kondrich <kondrich@openai.com>
2023-05-17 18:23:54 -0700

    move request timeout logic inside openai completionfn

--- evals/utils/api_utils.py
+++ evals/utils/api_utils.py
@@ -1,10 +1,12 @@
 """
 This file defines various helper functions for interacting with the OpenAI API.
 """
+import concurrent
 import logging
 
 import backoff
 import openai
+
 
 @backoff.on_exception(
     wait_gen=backoff.expo,
@@ -30,6 +32,20 @@ def openai_completion_create_retrying(*args, **kwargs):
     return result
 
 
+def request_with_timeout(func, *args, timeout=5, **kwargs):
+    """
+    Worker thread for making a single request within allotted time.
+    """
+    while True:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+            future = executor.submit(func, *args, **kwargs)
+            try:
+                result = future.result(timeout=timeout)
+                return result
+            except concurrent.futures.TimeoutError as e:
+                continue
+
+
 @backoff.on_exception(
     wait_gen=backoff.expo,
     exception=(
@@ -47,7 +63,7 @@ def openai_chat_completion_create_retrying(*args, **kwargs):
     Helper function for creating a chat completion.
     `args` and `kwargs` match what is accepted by `openai.ChatCompletion.create`.
     """
-    result = openai.ChatCompletion.create(*args, **kwargs)
+    result = request_with_timeout(openai.ChatCompletion.create, *args, **kwargs)
     if "error" in result:
         logging.warning(result)
         raise openai.error.APIError(result["error"])
