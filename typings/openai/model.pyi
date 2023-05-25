1a4c25039178f2f0643bf063bfaf936dc4549236
pan93412 <pan93412@gmail.com>
2023-05-25 13:56:26 +0800

    Use Optional syntax in typings

--- typings/openai/model.pyi
+++ typings/openai/model.pyi
@@ -1,13 +1,15 @@
+from typing import Optional
+
 from .response import ListResponse
 
 class Model:
     @classmethod
     def list(
         cls,
-        api_key: str | None = ...,
-        request_id: str | None = ...,
-        api_version: str | None = ...,
-        organization: str | None = ...,
-        api_base: str | None = ...,
-        api_type: str | None = ...,
+        api_key: Optional[str] = ...,
+        request_id: Optional[str] = ...,
+        api_version: Optional[str] = ...,
+        organization: Optional[str] = ...,
+        api_base: Optional[str] = ...,
+        api_type: Optional[str] = ...,
     ) -> ListResponse: ...
