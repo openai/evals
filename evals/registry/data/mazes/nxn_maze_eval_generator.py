c81f44ed772b6da52cd18fc36f4f86f2c7b01014
aitechforgeCEO <douglas.monsky@gmail.com>
2023-06-10 01:24:56 -0400

    Improve Model Reasoning, Evaluation and Output Formatting
    In response to the feedback provided, this commit incorporates three main changes:
    
    1. Model Reasoning: The model is now prompted to reason before generating a response to complex tasks. This enhancement gives the model a better opportunity to solve puzzles or tasks by understanding and processing the underlying steps.
    
    2. Evaluation Method: The Includes evaluation method is now integrated into our testing process. This change should improve the evaluation of model answers, providing more reliable performance metrics.
    
    3. Output Formatting: The model now outputs in a more structured format, with the final answer being enclosed in square brackets (e.g., [up, up, right]). This standardized formatting simplifies the identification and evaluation of final answers.
    
    Performance improvements were noted across various test sets after implementing these changes. Here are the comparative scores:
    
    - 3x3-singlemove: GPT-3.5-Turbo =42%, GPT-4 = 57%
    - 4x4-singlemove: GPT-3.5-Turbo =42%, GPT-4 = 58%
    - 10x10-singlemove: GPT-3.5-Turbo =40%, GPT-4 = 38%
    - 3x3: GPT-3.5-Turbo = 26%, GPT-4 = 33%
    - 4x4: GPT-3.5-Turbo =33%, GPT-4 = 36%
    - 10x10: GPT-3.5-Turbo =13%, GPT-4 = 15%
    
    In addition, a feature has been added to the nxn_maze_eval_generator to generate maze plots as PNG files for potential multimodal evaluations in the future.

--- evals/registry/data/mazes/nxn_maze_eval_generator.py
+++ evals/registry/data/mazes/nxn_maze_eval_generator.py
@@ -165,6 +165,7 @@ def generate_example_files(
     maze_height: int,
     output_directory: str = ".",
     show_plot: bool = False,
+    save_plot: bool = False,
     verbose: bool = False,
 ) -> None:
     """
@@ -176,8 +177,10 @@ def generate_example_files(
         maze_height (int): The height of the maze.
         output_directory (str, optional): The directory to export the maze examples to. Defaults to ".".
         show_plot (bool, optional): Whether to display the maze plot using matplotlib. Defaults to False.
+        save_plot (bool, optional): Whether to save the maze plot to a file. Defaults to False.
         verbose (bool, optional): Whether to print as threads finish. Defaults to False.
     """
+
 
     def create_example(progress_bar: tqdm) -> ProgressUpdate:
         """
@@ -249,7 +252,7 @@ def generate_example_files(
     if show_plot:
         for future in concurrent.futures.as_completed(futures):
             _, _, maze = future.result()
-            plot_maze(maze)
+            plot_maze(maze, show=show_plot, save_img=save_plot)
 
 
 def create_move_line(
@@ -266,33 +269,81 @@ def create_move_line(
     Returns:
         Dict[str, Union[List[Dict[str, str]], str]]: A dictionary containing the move line.
     """
-    task_description = (
-        f"Task: You will be given a maze, determine the shortest sequence of"
-        f" moves to reach the end without walking through walls. Respond only with the "
-        f"{'first move' if first_move_only else 'a comma seperated list of moves (example_output:down, right, up, left, left)'} "
-        f"and do not include any additional information. The maze elements are:"
-        f" pathways (0), walls (1), your position (2), and exit (3). "
-        f'Moves that go outside the maze are illegal. valid_moves:["right", "left", "up", "down"]'
-    )
+    task_description = f"""**Task Description:**
+<task_description>
+You will be given a text-based 2D maze represented by numbers. The maze consists of pathways (0), walls (1), a starting point (2), and an exit (3).
+Your task is to determine the {'correct first move'if first_move_only else'full solution (series of moves) to solve the maze'}, based on the following rules:
+- You can move up, down, left, or right to the immediate next cell in the chosen direction. The directions are always 
+considered from a top-down view of the maze: regardless of your previous move 'up' moves to the cell above (True North), 
+'down' to the cell below, 'left' to the cell on the left, and 'right' to the cell on the right.
+- Moves that lead to a wall (1) or outside the maze boundaries are not allowed.
+- The goal is to reach the exit (3) from the starting point (2) using the shortest possible legal path.
+- When considering potential moves, exclude directions that lead out of the maze boundaries or back to a previously visited location.
+</task_description-end>
+**Instructions:**
+<instructions>
+1. Write down the maze in its current form.
+2. Make some initial inferences based on the layout of the maze, the location of the starting point (2), the exit (3), and the walls (1).
+3. Identify possible moves from the start position (2) [up, down, left, right], eliminate those that lead to walls or outside the maze. Print the remaining options.
+4. Work through a logical chain of thought to propose a solution to the maze. This should involve identifying the 
+possible moves from the current position and eliminating those that lead to walls or outside the maze.
+5. Critically evaluate your solution, identify any flaws in your decision-making, and adjust the solution if needed.
+{''if first_move_only else'6. Repeat steps 1 and 5 until you reach the exit (3).'} 
+<instructions-end>
+
+<example>
+For example, you may be given the following maze:
+<maze>
+[0,1,0]\n[0,1,0]\n[0,2,3] 
+<maze-end>
+
+<solution-formating>
+The final answer should be in exactly this format:
+{'Solution: [up] **GIVE ONLY THE FIRST STEP** do not inlucde more then one item inside the brackets' if first_move_only else 'Solution: [up,up,left]'}
+<solution-formating-end>
+
+<example-response>
+**Expected Response:**
+1) reprint the maze as it is given to us: [0,1,0]\n[0,1,0]\n[0,2,3] 
+2) the exit appears to be directly to the right of the start position,
+3) the possible moves from the start position are: [right, left] because down goes out of bounds, and up runs into a wall.
+4) the solution is: [right] because left moves away from the exit (3) and right directly leads to the exit.
+5) the solution is correct, no flaws in decision making.
+Solution: [right]
+<example-response-end>
+
+<example-end>
+
+Here is your maze. Please solve it:
+<maze-to-solve>
+{maze_repr}
+<maze-to-solve-end>
+
+Your Notes and thoughts:
+Your Solution:
+"""
+
 
     return {
         "input": [
-            {"role": "system", "content": task_description},
-            {"role": "user", "content": maze_repr},
+            {"role": "user", "content": task_description},
         ],
-        "ideal": (moves[0] if first_move_only else ", ".join(moves))
+        "ideal": (f"[{moves[0]}]" if first_move_only else f'{", ".join(moves)}')
         if moves
         else "No valid path",
     }
 
 
-def plot_maze(maze: np.ndarray) -> None:
+def plot_maze(maze: np.ndarray, show=False, save_img=False) -> None:
     """
     Plots a maze using matplotlib.
 
     Args:
         maze (np.ndarray): A NumPy array representing the maze.
+        show (bool): Show the plot if True, default is False.
+        save_img (bool): Save the plot as an image if True, default is False.
     """
+    import os
     import matplotlib.pyplot as plt
     from matplotlib.patches import Patch
     from matplotlib.colors import ListedColormap
@@ -313,16 +364,28 @@ def plot_maze(maze: np.ndarray) -> None:
     ]
 
     plt.imshow(maze, cmap=cmap)
-    plt.legend(handles=legend_elements)
-    plt.show()
+    plt.legend(handles=legend_elements, bbox_to_anchor=(1.05, 1), loc='upper left')
+    if save_img:
+        output_folder = "images"
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        height, width = maze.shape
+        file_path = os.path.join(output_folder, f"maze-{height}x{width}-{len(os.listdir(output_folder))}.png")
+        plt.savefig(file_path)
+
+    if show:
+        plt.show()
+
+    plt.clf()
 
 
 if __name__ == "__main__":
     num_examples = 2000
-    maze_width = 5
-    maze_height = 5
+    maze_width = 3
+    maze_height = 3
     output_directory = ""
     show_plot = False
+    save_plot = False
     verbose = False
     generate_example_files(
         num_examples,
@@ -330,5 +393,6 @@ if __name__ == "__main__":
         maze_height,
         output_directory=output_directory,
         show_plot=show_plot,
+        save_plot=save_plot,
         verbose=verbose,
     )
