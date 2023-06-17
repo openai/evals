fde504169eb2eb6b6e9b6ef1efc5831a1020d204
Kyrylo Kalashnikov <kir486680@gmail.com>
2023-06-17 01:45:28 +0000

    implemented base memory

--- evals/memory/base_memory.py
+++ evals/memory/base_memory.py
@@ -1,17 +1,37 @@
-"""
-Base class for all memory data structures.
-"""
 import abc
 
 class Memory(abc.ABC):
+    """
+    Base class for all memory data structures.
+    This class defines the interface for a memory module.
+    """
+
     @abc.abstractmethod
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the memory module.
+        """
         pass
 
     @abc.abstractmethod
-    def create(self, text):
+    def create(self, *args, **kwargs):
+        """
+        Add a new text entry to the memory.
+        
+        Args:
+            text (str): The text to add to the memory.
+        """
         pass 
 
     @abc.abstractmethod
-    def sample(self, query):
+    def sample(self, *args, **kwargs):
+        """
+        Sample an entry from the memory based on a query.
+        
+        Args:
+            query (str): The query to use for sampling from the memory.
+        
+        Returns:
+            str: A text entry from the memory.
+        """
         pass
