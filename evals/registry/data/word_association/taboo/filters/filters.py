e8bd18ccf544066e89f921b26abc99aa79f3e8d1
aitechforgeCEO <douglas.monsky@gmail.com>
2023-06-23 22:42:35 -0400

    refactor(filters): simplify filter functions, remove classes (word-association-eval)

--- evals/registry/data/word_association/taboo/filters/filters.py
+++ evals/registry/data/word_association/taboo/filters/filters.py
@@ -1,37 +1,17 @@
 import nltk
-from abc import ABC, abstractmethod
 
 
-class Filter(ABC):
-    @abstractmethod
-    def __call__(self, words: list[str]) -> list[str]:
-        raise NotImplementedError
+def parts_of_speech_filter(words: list[str], parts_of_speech: list[str]) -> list[str]:
+    return [word for word in words if word[1] in parts_of_speech]
 
 
-class PartsOfSpeechFilter(Filter):
-    def __init__(self, parts_of_speech: list[str]) -> None:
-        self.parts_of_speech = parts_of_speech
-
-    def __call__(self, words: list[str]) -> list[str]:
-        return [word for word in words if word[1] in self.parts_of_speech]
-
-
-class FrequencyFilter(Filter):
-    def __init__(self, frequency_distribution: nltk.FreqDist, threshold: int) -> None:
-        self.frequency_distribution = frequency_distribution
-        self.threshold = threshold
-
-    def __call__(self, words: list[str]) -> list[str]:
-        return [
-            word
-            for word in words
-            if self.frequency_distribution[word] >= self.threshold
-        ]
+def frequency_filter(words: list[str], frequency_distribution: nltk.FreqDist, threshold: int) -> list[str]:
+    return [
+        word
+        for word in words
+        if frequency_distribution[word] >= threshold
+    ]
 
 
-class SubwordFilter(Filter):
-    def __init__(self, subword: str) -> None:
-        self.subword = subword
-
-    def __call__(self, words: list[str]) -> list[str]:
-        return [word for word in words if self.subword not in word]
+def subword_filter(words: list[str], subword: str) -> list[str]:
+    return [word for word in words if subword not in word]
