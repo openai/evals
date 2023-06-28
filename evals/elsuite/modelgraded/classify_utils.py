f063d22374b2fe79527043f11213535294943c8b
Douglas Monsky <douglas.monsky@gmail.com>
2023-06-28 07:20:08 -0400

    feat(evals): refactor modelgraded evaluation logic and update yaml files (#model-graded-refactor)
    This commit includes changes to the modelgraded evaluation logic in the classify_utils.py file to fix the high rate of invalid answers from model graded evals. It also updates the choice_strings format in multiple yaml files under the modelgraded directory. The choice_strings in model-graded yamls are now standardized to be listed as separate items instead of a single string. This change improves readability and consistency across the codebase. Finally, minor improvements to documentation in classify_utils.py.

--- evals/elsuite/modelgraded/classify_utils.py
+++ evals/elsuite/modelgraded/classify_utils.py
@@ -1,6 +1,7 @@
+import re
 import logging
 import string
-from typing import Any, Callable, Iterable, Optional, Union
+from typing import Any, Callable, Iterable, Optional, Union, Tuple, List, Dict
 
 from evals import CompletionFn
 from evals.elsuite.modelgraded.base import ModelGradedSpec
@@ -9,23 +10,32 @@ from evals.prompt.base import OpenAICreateChatPrompt, is_chat_prompt
 
 INVALID_STR = "__invalid__"
 
-
+# Mapping of evaluation types to their corresponding prompt formats
 ANSWER_PROMPTS = {
-    # e.g. "Yes"
-    "classify": "Answer the question by printing only a single choice from {choices} (without quotes or punctuation) corresponding to the correct answer with no other text.".strip(),
-    # e.g. "Yes\n The reasons are: ..."
-    "classify_cot": "First, answer by printing a single choice from {choices} (without quotes or punctuation) corresponding to the correct answer. Then, from the next line, explain your reasonings step by step.".strip(),
-    # e.g. "Let's think step by step. ...\nYes"
-    "cot_classify": """
-First, write out in a step by step manner your reasoning to be sure that your conclusion is correct. Avoid simply stating the correct answer at the outset. Then print only a single choice from {choices} (without quotes or punctuation) on its own line corresponding to the correct answer. At the end, repeat just the answer by itself on a new line.
-
-Reasoning:""".strip(),
-    "cot_classify_jp": """
-まず、一歩一歩あなたの推論を書き出してください。単に正しい答えを最初に述べることを避けてください。次に、{choices}（引用符や句読点なし）から正しい答えに対応する1つの選択肢を単独の行に書きだしてください。最後に、答えだけを新しい行に繰り返してください。
-
-推論：
-    """.strip(),
+    # choose answer only
+    "classify": "Answer the question by printing only a single answer choice from {choices}."
+    "\nYour final output should be in the following format (NOTE: Include the square brackets):"
+    "\nFinal Answer: [<choice>]",
+    # choose answer first, then explain reasoning
+    "classify_cot": "First, choose a single answer choice from {choices}."
+    "Then, on the next line, explain your reasonings step by step."
+    "\nYour final output should be in the following format (NOTE: Include the square brackets):"
+    "\nFinal Answer: [<choice>]\nReasoning: <your reasoning>",
+    # explain reasoning first, then choose answer
+    "cot_classify": "First,  explain your reasonings step by step. "
+    "Then, on the next line, choose a single answer choice from {choices}."
+    "\nYour final output should be in the following format (NOTE: Include the square brackets):"
+    "\nReasoning: <your reasoning>\nFinal Answer: [<choice>]",
+    # explain reasoning first, then choose answer (Japanese)
+    "cot_classify_jp": "まず、一歩一歩あなたの推論を書き出してください。単に正しい答えを最初に述べることを避けてください。次に、{choices}"
+    "（引用符や句読点なし）から正しい答えに対応する1つの選択肢を単独の行に書きだしてください。"
+    "最後に、答えだけを新しい行に繰り返してください。推論："
+    # If translating to Japanese, it is important to update the regex get_choice() function below
+    "\nYour final output should be in the following format (NOTE: Include the square brackets):"
+    "\nReasoning: <your reasoning>\nFinal Answer: [<choice>]",
 }
+
+# Mapping of match function identifiers to actual functions
 MATCH_FNS = {
     "include": lambda x, y: float(x in y),
     "exact": lambda x, y: float(x == y),
@@ -34,8 +44,10 @@ MATCH_FNS = {
 }
 
 
-def get_choice_strings(choice_strings: Union[list[str], str], n: Optional[int] = None):
-    # 'choice_strings' is a list of strings that specifies the possible choices
+def get_choice_strings(
+    choice_strings: Union[List[str], str], n: Optional[int] = None
+) -> List[str]:
+    """Converts 'choice_strings' input to list of strings representation."""
     if choice_strings == "from_n":
         choice_strings = [str(i + 1) for i in range(n)]
     elif choice_strings == "from_n_abc":
@@ -51,12 +63,13 @@ def get_choice_strings(choice_strings: Union[list[str], str], n: Optional[int] =
 def classify(
     mg: ModelGradedSpec,
     completion_fn: CompletionFn,
-    completion_kwargs: Optional[dict[str, Any]] = None,
-    format_kwargs: Optional[dict[str, Any]] = None,
+    completion_kwargs: Optional[Dict[str, Any]] = None,
+    format_kwargs: Optional[Dict[str, Any]] = None,
     eval_type: Optional[str] = None,
     n: Optional[int] = None,
     match_fn: str = "starts_or_endswith",
-) -> str:
+) -> Tuple[str, Dict[str, Any]]:
+    """Performs classification by evaluating model output and comparing it to possible choices."""
     completion_kwargs = completion_kwargs or {}
     format_kwargs = format_kwargs or {}
 
@@ -67,13 +80,14 @@ def classify(
     prompt = mg.prompt
     if isinstance(prompt, str):
         prompt = [{"role": "user", "content": prompt}]
-    if eval_type:
-        prompt = append_answer_prompt(
-            prompt=prompt,
-            eval_type=eval_type,
-            choice_strings=choice_strings,
-        )
 
+    if eval_type is None:
+        eval_type = "classify"
+    prompt = append_answer_prompt(
+        prompt=prompt,
+        eval_type=eval_type,
+        choice_strings=choice_strings,
+    )
     evaluate = PromptFn(prompt, completion_fn=completion_fn, **completion_kwargs)
     evaluation, prompt = evaluate(n=n, **format_kwargs)
     choice = get_choice(evaluation, mg.eval_type or eval_type, match_fn, choice_strings)
@@ -89,8 +103,9 @@ def classify(
 def get_choice_score(
     choice: str,
     choice_strings: Iterable[str],
-    choice_scores: Optional[Union[dict[str, float], str]] = None,
+    choice_scores: Optional[Union[Dict[str, float], str]] = None,
 ) -> Optional[float]:
+    """ "Calculates the score of a specific choice."""
     if choice_scores is None:
         return None
     if choice_scores == "from_strings":
@@ -102,28 +117,36 @@ def get_choice_score(
 
 
 def choice_to_str(choice_strings: Iterable[str]) -> str:
-    """Return a string of choices, e.g. '"Yes" or "No" or "Maybe"'."""
+    """Converts iterable of choice strings into a human-readable string."""
     return " or ".join(f'"{choice}"' for choice in choice_strings)
 
 
 def get_choice(
-    text: str, eval_type: str, match_fn: Union[str, Callable], choice_strings: Iterable[str]
+    text: str,
+    eval_type: str,
+    match_fn: Union[str, Callable],
+    choice_strings: Iterable[str],
 ) -> str:
-    """Clean the answer string to a choice string to one of choice_strings. Return '__invalid__.' if no match."""
+    """Extracts the final choice from the model output and validates it against the available choices.
+    Return INVALID_STR if no match."""
     if isinstance(match_fn, str):
-        match_fn = MATCH_FNS[match_fn]
-    lines = text.strip().split("\n")
-    if eval_type.startswith("cot_classify"):
-        lines = lines[::-1]  # reverse lines
-    for line in lines:
-        line = line.strip()
-        line = "".join(c for c in line if c not in string.punctuation)
-        if not line:
-            continue
+        try:
+            match_fn = MATCH_FNS[match_fn]
+        except KeyError:
+            raise ValueError(f"match_fn {match_fn} is not valid")
+
+    match = re.search(r"Final Answer: \[(.+?)]", text)
+    if match:
+        model_choice = "".join(
+            char for char in match.group(1) if char not in string.punctuation
+        )
         for choice in choice_strings:
-            if match_fn(line, choice):
+            if match_fn(model_choice, choice):
+                logging.debug(
+                    f"Matched {model_choice} to {choice} for {eval_type}: {text}"
+                )
                 return choice
-    logging.warn(f"Choices {choice_strings} not parsable for {eval_type}: {text}")
+    logging.warning(f"Choices {choice_strings} not parsable for {eval_type}: {text}")
     return INVALID_STR
 
 
@@ -134,17 +157,23 @@ def append_answer_prompt(
     answer_prompt: Optional[OpenAICreateChatPrompt] = None,
     choice_strings: Optional[Iterable[str]] = None,
 ) -> OpenAICreateChatPrompt:
-    """Append answer prompt to prompt."""
+    """Appends evaluation instructions to the prompt for the model."""
     answer_prompt = answer_prompt or ANSWER_PROMPTS[eval_type]
     answer_prompt = format_prompt(answer_prompt, choices=choice_to_str(choice_strings))
     if append_type == "as_content":
-        assert isinstance(answer_prompt, str), f"prompt must be str, not {type(answer_prompt)}"
+        assert isinstance(
+            answer_prompt, str
+        ), f"prompt must be str, not {type(answer_prompt)}"
         prompt[-1]["content"] += "\n\n" + answer_prompt
     elif append_type == "as_message":
-        assert is_chat_prompt(answer_prompt), f"prompt must be chat prompt, not {answer_prompt}"
+        assert is_chat_prompt(
+            answer_prompt
+        ), f"prompt must be chat prompt, not {answer_prompt}"
         prompt += answer_prompt
     else:
-        raise ValueError(f"append_type must be 'as_content' or 'as_message', not {append_type}")
+        raise ValueError(
+            f"append_type must be 'as_content' or 'as_message', not {append_type}"
+        )
     return prompt
 
 
@@ -153,8 +182,9 @@ def sample_and_concat_n_completions(
     prompt: OpenAICreateChatPrompt,
     n: int,
     template_i: str,
-    sample_kwargs: dict,
-):
+    sample_kwargs: Dict[str, Any],
+) -> str:
+    """Samples and concatenates multiple completions using either single or multiple models."""
     assert template_i
     completion_i_s = []
     for i in range(n):
@@ -165,14 +195,16 @@ def sample_and_concat_n_completions(
         else:
             # use the single model for all completions
             completion_fn = completion_fns[0]
-        get_input_completion = PromptFn(prompt, completion_fn=completion_fn, **sample_kwargs)
+        get_input_completion = PromptFn(
+            prompt, completion_fn=completion_fn, **sample_kwargs
+        )
         completion_i, _ = get_input_completion()
         completion_i_s.append(completion_i)
     return concat_n_completions(completion_i_s, template_i=template_i)
 
 
 def concat_n_completions(completions: Iterable[str], template_i: str) -> str:
-    """Concatenate n completions into a single text string."""
+    """Combines multiple completions into a single formatted string."""
     completion = ""
     for i, completion_i in enumerate(completions):
         completion += format_necessary(
