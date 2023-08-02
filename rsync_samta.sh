b2201e7d0b0050bce4a9d9d33a0c473170c35578
Samta Kamboj <samta.kamboj@g42.ai>
2023-08-02 13:25:00 +0400

    newline fix

--- rsync_samta.sh
+++
@@ -1,2 +0,0 @@
-# rsync -urav --exclude='.git' ./ sk4@172.31.20.116:/home/sk4/projects/language_model/git_code
-rsync -urav --exclude='.git' ./ ext_samta.kamboj@172.31.14.3:/nfs/users/ext_samta.kamboj/softwares/evals
