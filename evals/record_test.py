openai/evals

commit 981ff4b7984763f70292d78688634b74d04efe2c

Update LocalRecorder to filter data fields before recording (#1330)
There are some cases where we do not actually want to log the events
themselves. For example, if we are evaluating against data that may
contain PII.

evals/record.py
@@ -14,7 +14,7 @@ import threading
 import time
 from contextvars import ContextVar
 from datetime import datetime, timezone
-from typing import Any, List, Optional, Sequence
+from typing import Any, List, Optional, Sequence, Text
 
 import blobfile as bf
 import requests
@@ -310,9 +310,27 @@ class LocalRecorder(RecorderBase):
     This is the default recorder used by `oaieval`.
     """
 
-    def __init__(self, log_path: Optional[str], run_spec: RunSpec):
+    def __init__(self,
+        log_path: Optional[str],
+        run_spec: RunSpec,
+        hidden_data_fields: Sequence[Text] = []):
+        """
+        Initializes a LocalRecorder.
+
+        Args:
+            log_path (Optional[str]): Path to which the LocalRecorder will 
+                record events. Currently accepts local paths, google cloud 
+                storage paths, or Azure blob paths.
+            run_spec (RunSpec): Passed to the superclass to provide metadata 
+                about the current evals run.
+            hidden_data_fields (Sequence[Text]): Fields to avoid writing in the
+                output. This is particularly useful when using a language model
+                as an evaluator of sensitive customer data which should not be
+                written to disc.
+        """
         super().__init__(run_spec)
         self.event_file_path = log_path
+        self.hidden_data_fields = hidden_data_fields
         if log_path is not None:
             with bf.BlobFile(log_path, "wb") as f:
                 f.write((jsondumps({"spec": dataclasses.asdict(run_spec)}) + "\n").encode("utf-8"))
@@ -320,7 +338,7 @@ class LocalRecorder(RecorderBase):
     def _flush_events_internal(self, events_to_write: Sequence[Event]):
         start = time.time()
         try:
-            lines = [jsondumps(event) + "\n" for event in events_to_write]
+            lines = [jsondumps(event, exclude_keys=hidden_data_fields) + "\n" for event in events_to_write]
         except TypeError as e:
             logger.error(f"Failed to serialize events: {events_to_write}")
             raise e

evals/data.py
@@ -11,7 +11,7 @@ import os
 import urllib
 from collections.abc import Iterator
 from functools import partial
-from typing import Any, Sequence, Union
+from typing import Any, List, Optional, Sequence, Text, Union
 
 import blobfile as bf
 import lz4.frame
@@ -162,28 +162,44 @@ def get_csv(path, fieldnames=None):
         return [row for row in reader]
 
 
-def _to_py_types(o: Any) -> Any:
+def _to_py_types(o: Any, exclude_keys: List[Text]) -> Any:
     if isinstance(o, dict):
-        return {k: _to_py_types(v) for k, v in o.items()}
+        return {
+            k: _to_py_types(v, exclude_keys=exclude_keys) for k, v in o.items() if k not in exclude_keys}
+
     if isinstance(o, list):
-        return [_to_py_types(v) for v in o]
+        return [_to_py_types(v, exclude_keys=exclude_keys) for v in o]
 
     if dataclasses.is_dataclass(o):
-        return _to_py_types(dataclasses.asdict(o))
+        return _to_py_types(dataclasses.asdict(o), exclude_keys=exclude_keys)
 
     # pydantic data classes
     if isinstance(o, pydantic.BaseModel):
-        return json.loads(o.json())
+        return {
+            k: _to_py_types(v, exclude_keys=exclude_keys)
+            for k, v in json.loads(o.json()).items()
+            if k not in exclude_keys
+        }
 
     return o
 
 
 class EnhancedJSONEncoder(json.JSONEncoder):
+    def __init__(self, exclude_keys: Optional[List[Text]]=None, **kwargs: Any):
+        super().__init__(**kwargs)
+        self.exclude_keys = exclude_keys if exclude_keys else []
+
     def default(self, o: Any) -> str:
-        return _to_py_types(o)
+        return _to_py_types(o, self.exclude_keys)
 
 
 def jsondumps(o: Any, ensure_ascii: bool = False, **kwargs: Any) -> str:
+    # The JSONEncoder class's .default method is only applied to dictionary values,
+    # not keys. In order to exclude keys from the output of this jsondumps method
+    # we need to exclude them outside the encoder.
+    if isinstance(o, dict) and "exclude_keys" in kwargs:
+        for key in kwargs["exclude_keys"]:
+            del o[key]
     return json.dumps(o, cls=EnhancedJSONEncoder, ensure_ascii=ensure_ascii, **kwargs)
 
 

evals/data_test.py
@@ -1,0 +1,27 @@
+import ast
+import dataclasses
+
+from pydantic import BaseModel
+from typing import Text, Optional
+from evals.data import jsondumps
+
+
+class MyPydanticClass(BaseModel):
+    first_name: Text
+    last_name: Text
+
+
+@dataclasses.dataclass
+class MyDataClass:
+    first_name: Text
+    last_name: Text
+    sub_class: Optional[MyPydanticClass] = None
+
+def test_jsondumps():
+    assert "{\"first_name\": \"a\", \"last_name\": \"b\", \"sub_class\": null}" == jsondumps(MyDataClass(first_name="a", last_name="b"))
+    assert "{\"first_name\": \"a\", \"sub_class\": null}" == jsondumps(MyDataClass(first_name="a", last_name="b"), exclude_keys=["last_name"])
+    assert "{\"first_name\": \"a\", \"last_name\": \"b\"}" == jsondumps(MyPydanticClass(first_name="a", last_name="b"))
+    assert "{\"first_name\": \"a\"}" == jsondumps(MyPydanticClass(first_name="a", last_name="b"), exclude_keys=["last_name"])
+    assert "{\"first_name\": \"a\", \"last_name\": \"b\"}" == jsondumps({"first_name": "a", "last_name": "b"})
+    assert "{\"first_name\": \"a\"}" == jsondumps({"first_name": "a", "last_name": "b"}, exclude_keys=["last_name"])
+    assert "{\"first_name\": \"a\", \"sub_class\": {\"first_name\": \"a\"}}" == jsondumps(MyDataClass("a", "b", MyPydanticClass(first_name="a", last_name="b")), exclude_keys=["last_name"])

commit 0f344ba404c6a90d15e96dc58cf5f76fe241e20d

Fixes a bug in LocalRecorder
Fixes a bug introduced in
https://github.com/openai/evals/commit/981ff4b7984763f70292d78688634b74d04efe2c
in which `hidden_data_fields` was referenced without the `self` keyword.
This also introduces a recorder test to ensure it doesn't happen again.

evals/record.py
@@ -338,7 +338,7 @@ class LocalRecorder(RecorderBase):
     def _flush_events_internal(self, events_to_write: Sequence[Event]):
         start = time.time()
         try:
-            lines = [jsondumps(event, exclude_keys=hidden_data_fields) + "\n" for event in events_to_write]
+            lines = [jsondumps(event, exclude_keys=self.hidden_data_fields) + "\n" for event in events_to_write]
         except TypeError as e:
             logger.error(f"Failed to serialize events: {events_to_write}")
             raise e

evals/record_test.py
@@ -1,0 +1,26 @@
+import json
+import tempfile
+from evals.record import LocalRecorder
+from evals.base import RunSpec
+
+
+def test_passes_hidden_data_field_to_jsondumps() -> None:
+    tmp_file = tempfile.mktemp()
+    spec = RunSpec(
+        completion_fns=[""],
+        eval_name="",
+        base_eval="",
+        split="",
+        run_config={},
+        created_by="",
+        run_id="",
+        created_at=""
+    )
+    local_recorder = LocalRecorder(tmp_file, spec, ["should_be_hidden"])
+    local_recorder.record_event("raw_sample", {"should_be_hidden": 1, "should_not_be_hidden": 2}, sample_id="test")
+    local_recorder.flush_events()
+    with open(tmp_file, "r", -1, "utf-8") as f:
+        first_line = f.readline()
+        assert len(first_line) > 0
+        second_line = json.loads(f.readline())
+        assert second_line["data"] == {"should_not_be_hidden": 2}