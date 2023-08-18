c88a233d357e287c861aa5fc5c50b2126e74e00f
Mike Alvarino <michael.alvarino@gmail.com>
2023-08-18 14:19:48 -0400

    Fixes a bug in LocalRecorder (#1337)
    Fixes a bug introduced in
    
    https://github.com/openai/evals/commit/981ff4b7984763f70292d78688634b74d04efe2c
    in which `hidden_data_fields` was referenced without the `self` keyword.
    This also introduces a recorder test to ensure it doesn't happen again.

---
+++ evals/record_test.py
@@ -0,0 +1,26 @@
+import json
+import tempfile
+from evals.record import LocalRecorder
+from evals.base import RunSpec
+
+
+def test_passes_hidden_data_field_to_jsondumps() -> None:
+    tmp_file = tempfile.mktemp()
+    spec = RunSpec(
+        completion_fns=[""],
+        eval_name="",
+        base_eval="",
+        split="",
+        run_config={},
+        created_by="",
+        run_id="",
+        created_at=""
+    )
+    local_recorder = LocalRecorder(tmp_file, spec, ["should_be_hidden"])
+    local_recorder.record_event("raw_sample", {"should_be_hidden": 1, "should_not_be_hidden": 2}, sample_id="test")
+    local_recorder.flush_events()
+    with open(tmp_file, "r", -1, "utf-8") as f:
+        first_line = f.readline()
+        assert len(first_line) > 0
+        second_line = json.loads(f.readline())
+        assert second_line["data"] == {"should_not_be_hidden": 2}
