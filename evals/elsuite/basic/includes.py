368ee8b48413330f740532fdd4741fbe133d98d6
sjadler2004 <sjadler2004@gmail.com>
2023-10-17 15:23:16 -0700

    add exclusions to includes.py

--- evals/elsuite/basic/includes.py
+++ evals/elsuite/basic/includes.py
@@ -12,6 +12,7 @@ class Includes(evals.Eval):
         completion_fns: list[CompletionFn],
         samples_jsonl: str,
         ignore_case: bool = False,
+        use_exclusions: bool = False,
         *args,
         **kwargs,
     ):
@@ -19,11 +20,14 @@ class Includes(evals.Eval):
         assert len(completion_fns) == 1, "Includes only supports one completion fn"
         self.samples_jsonl = samples_jsonl
         self.ignore_case = ignore_case
+        self.use_exclusions = use_exclusions
 
     def eval_sample(self, sample: Any, *_):
         assert isinstance(sample, dict), "sample must be a dict"
         assert "input" in sample, "sample must have an 'input' key"
         assert "ideal" in sample, "sample must have an 'ideal' key"
+        if self.use_exclusions:
+            assert "exclude" in sample, "sample must have an 'exclude' key if use_exclusions is set to be True"
 
         prompt = sample["input"]
         result = self.completion_fn(
@@ -42,8 +46,17 @@ class Includes(evals.Eval):
         includes_answer = any(
             [utils.get_answer(sampled, ref, self.ignore_case) is not None for ref in ideal]
         )
+
+        if self.use_exclusions:
+            
+            # if any exclude_item is present, this makes the answer False, regardless of presence of any/all include items
+            if any(exclude_item in sampled for exclude_item in self.exclude):
+                includes_answer = False
+            
+            excluded_item_found = True
+        
         evals.record.record_match(
-            includes_answer, expected=sample["ideal"], picked=sampled, sampled=sampled
+            includes_answer, expected=sample["ideal"], picked=sampled, sampled=sampled, excluded_item_found=excluded_item_found,
         )
         return includes_answer
 
