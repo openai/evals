11fe08c0083d64cfdba538ffbb5d01ccc2f12f94
TablewareBox <1700011741@pku.edu.cn>
2024-03-06 16:53:16 +0800

    fix gemini

--- evals/completion_fns/gemini.py
+++ evals/completion_fns/gemini.py
@@ -10,13 +10,18 @@ from evals.prompt.base import (
     Prompt,
 )
 from evals.utils.api_utils import request_with_timeout
-from evals.utils.doc_utils import extract_text_and_fill_in_images, extract_text
+from evals.utils.doc_utils import extract_text_and_fill_in_images, extract_text, num_tokens_from_string
 from evals.record import record_sampling
 
 try:
     import google.generativeai as genai
 except ImportError:
     print("Run `pip install google-generativeai` to use Gemini API.")
+
+model_max_tokens = {
+    "gemini-pro": 20000,
+    "gemini-pro-vision": 20000,
+}
 
 
 class GeminiCompletionResult(CompletionResult):
@@ -33,14 +38,14 @@ class GeminiCompletionFn(CompletionFn):
         self,
         model: Optional[str] = "gemini-pro",
         api_base: Optional[str] = None,
-        api_keys: Optional[list[str]] = None,
+        api_keys: Union[list[str], str, None] = None,
         n_ctx: Optional[int] = None,
         extra_options: Optional[dict] = {},
         **kwargs,
     ):
         self.model = model
         self.api_base = api_base
-        self.api_keys = [api_keys] if type(api_keys) == str else [api_keys] or os.environ.get("GOOGLE_API_KEY").split(",")
+        self.api_keys = [api_keys] if type(api_keys) == str else os.environ.get("GOOGLE_API_KEY", "").split(",") or [api_keys]
         self.n_ctx = n_ctx
         self.extra_options = extra_options
 
@@ -67,6 +72,9 @@ class GeminiCompletionFn(CompletionFn):
             attached_file_content = ["The file is as follows:"]
             attached_file_content += extract_text_and_fill_in_images(kwargs["file_name"], None, False) \
                 if self.model == "gemini-pro-vision" else ["".join(extract_text(kwargs["file_name"]))]
+            max_tokens = model_max_tokens.get(self.model, 1000000)
+            while num_tokens_from_string(attached_file_content[1], "cl100k_base") > max_tokens:
+                attached_file_content[1] = attached_file_content[1][:-1000]
         else:
             attached_file_content = []
             self.model = "gemini-pro"
@@ -82,29 +90,39 @@ class GeminiCompletionFn(CompletionFn):
         safety_settings = [
             {
                 "category": "HARM_CATEGORY_HARASSMENT",
-                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+                "threshold": "BLOCK_NONE"
             },
             {
                 "category": "HARM_CATEGORY_HATE_SPEECH",
-                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+                "threshold": "BLOCK_NONE"
             },
             {
                 "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+                "threshold": "BLOCK_NONE"
             },
             {
                 "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+                "threshold": "BLOCK_NONE"
             },
         ]
         model = genai.GenerativeModel(model_name=self.model,
                                       generation_config=generation_config,
                                       safety_settings=safety_settings)
-        response = request_with_timeout(model.generate_content, contents=[openai_create_prompt] + attached_file_content)
-        # response = model.generate_content(
-        #     contents=[openai_create_prompt] + attached_file_content
-        # )
-        answer = response.text
+        # response = request_with_timeout(model.generate_content, contents=[openai_create_prompt] + attached_file_content)
+        response = model.generate_content(
+            contents=[openai_create_prompt] + attached_file_content,
+        )
+        # answer = response.text
+
+        try:
+            answer = response.text
+        except ValueError:
+            # if len(response.parts) > 0:
+            #     answer = response.parts[0].text
+            # else:
+            answer = ""
+        except:
+            answer = "None"
 
         record_sampling(prompt=prompt, sampled=answer)
         return GeminiCompletionResult(response=answer, prompt=prompt)
