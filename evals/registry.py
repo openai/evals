8f854d355978c0d20260c7cfe9b0de4993e30164
Pablo Androetto <androettop@gmail.com>
2024-05-17 09:21:05 -0300

    added model to make_plots and n_ctx fn

--- evals/registry.py
+++ evals/registry.py
@@ -42,6 +42,7 @@ def n_ctx_from_model_name(model_name: str) -> Optional[int]:
         ("gpt-3.5-turbo-", 4096),
         ("gpt-4-32k-", 32768),
         ("gpt-4-", 8192),
+        ("gpt-4o", 128_000),
     ]
     MODEL_NAME_TO_N_CTX: dict[str, int] = {
         "ada": 2048,
@@ -65,6 +66,7 @@ def n_ctx_from_model_name(model_name: str) -> Optional[int]:
         "gpt-4-1106-preview": 128_000,
         "gpt-4-turbo-preview": 128_000,
         "gpt-4-0125-preview": 128_000,
+        "gpt-4o": 128_000
     }
 
     # first, look for an exact match
