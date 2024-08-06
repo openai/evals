923e8392d8258e9c64cca4febe37a202ff4c6ca0
juberti <justin@fixie.ai>
2024-08-06 12:09:24 -0700

    sr

--- evals/elsuite/audio/run_experiments.sh
+++ evals/elsuite/audio/run_experiments.sh
@@ -1,14 +1,11 @@
 logdir=./logs
-outputdir=./outputs
-# export EVALS_THREADS=3
 
 timestamp=$(date +%Y%m%d_%H%M%S)
-logpathbase=$logdir/$timestamp/
+logpathbase=$logdir/$timestamp
+maxsamples=1000
 
 echo Running experiments and logging to $logpathbase
-oaievalset generation/direct/ultravox-v0.2 audio --record_path ${logpathbase}/payload.log --max_samples=100
+oaievalset generation/direct/ultravox-v0.2 audio --record_dir ${logpathbase} --max_samples=${maxsamples}
 
 echo Done running experiments, all logs in $logpathbase
-
-echo Producing plots, outputs to $outputdir
-#python make_plots.py --log_dir $logpathbase --out_dir $outputdir
+python evals/elsuite/audio/make_table.py --log_dir $logpathbase 
