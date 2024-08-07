ac35c57e1d259cf28a5dabf828d194d88fa31a70
Farzad Abdolhosseini <farzad@fixie.ai>
2024-08-07 13:19:39 -0700

    minor fixes

--- evals/solvers/providers/fixie/local_gpu_solver.py
+++ evals/solvers/providers/fixie/local_gpu_solver.py
@@ -13,28 +13,12 @@ import transformers
 from evals.solvers.solver import Solver, SolverResult
 from evals.task_state import TaskState
 
-
-@contextlib.contextmanager
-def run_on_master_first(is_master: bool):
-    """
-    If using DDP, allows the master process to run the enclosed code first.
-    This is useful when only one process should download a model or other resources first to avoid race conditions.
-    """
-    if is_master:
-        yield
-        if torch.distributed.is_initialized():
-            torch.distributed.barrier()
-    else:
-        # All other processes wait for the master to download the model first
-        if torch.distributed.is_initialized():
-            torch.distributed.barrier()
-        yield
+SAMPLE_RATE = 16000
 
 
 def solver_initializer(rank_queue: mp.Queue, world_size: int, model: str):
+    """Initializes the pipeline and the underlying model on the specified GPU."""
     print("is dist inited?", torch.distributed.is_initialized())
-    # torch.distributed.recv
-    # print("queue", queue)
     rank = rank_queue.get()
 
     if torch.cuda.is_available():
@@ -49,13 +33,18 @@ def solver_initializer(rank_queue: mp.Queue, world_size: int, model: str):
 
 
 def solver_worker(inputs: Dict[str, Any]):
-    global pipe
     return pipe(inputs, max_new_tokens=64)
 
 
 class LocalGPUSolver(Solver):
     """
-    TODO
+    A solver class for the locally running a model on multiple GPUs using a ProcessPoolExecutor.
+    The model is employed using a Hugging Face Transformers pipeline. We assume that the pipeline
+    is an UltravoxPipeline class and the inputs are text/audio messages.
+
+    Completion function parameters:
+    - model: str - The model name/path to use for the pipeline
+    - num_gpus: int - The number of GPUs to use for parallel processing (default: all available GPUs)
     """
 
     def __init__(
@@ -68,6 +57,7 @@ class LocalGPUSolver(Solver):
         self.completion_fn_options = completion_fn_options
 
         num_gpus = completion_fn_options.get("num_gpus", torch.cuda.device_count())
+        completion_fn_options.get("max_num_tokens", 64)
 
         if "model" not in completion_fn_options:
             raise ValueError("LocalGPUSolver requires a model to be specified.")
@@ -97,17 +87,19 @@ class LocalGPUSolver(Solver):
         ] + [msg.to_dict() for msg in task_state.messages]
 
         if not isinstance(msgs[-1]["content"], str):
+            # This means the last message is an audio message
             parts = msgs[-1]["content"]
             parts_str = [x["text"] if x["type"] == "text" else "<|audio|>" for x in parts]
+            # Concatenate all text parts into a single string
             msgs[-1]["content"] = "".join(parts_str)
             data_parts = [x["image_url"] for x in parts if x["type"] == "image_url"]
             assert len(data_parts) == 1
+            # Extract the audio data from the last message
             audio_data = data_parts[0]["url"].split(",")[1]
-            audio_data = base64.b64decode(audio_data)
-            audio_stream = io.BytesIO(audio_data)
+            audio_stream = io.BytesIO(base64.b64decode(audio_data))
 
-            # Read the audio data using soundfile
-            inputs["audio"] = librosa.load(audio_stream, sr=16000)[0]
+            # Read the audio data using soundfile and enforce the expected sample rate
+            inputs["audio"] = librosa.load(audio_stream, sr=SAMPLE_RATE)[0]
 
         inputs["turns"] = msgs
 
@@ -119,3 +111,20 @@ class LocalGPUSolver(Solver):
 
     def __del__(self):
         self.executor.shutdown()
+
+
+@contextlib.contextmanager
+def run_on_master_first(is_master: bool):
+    """
+    If using DDP, allows the master process to run the enclosed code first.
+    This is useful when only one process should download a model or other resources first to avoid race conditions.
+    """
+    if is_master:
+        yield
+        if torch.distributed.is_initialized():
+            torch.distributed.barrier()
+    else:
+        # All other processes wait for the master to download the model first
+        if torch.distributed.is_initialized():
+            torch.distributed.barrier()
+        yield
