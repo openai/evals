c28580ebb378e8bae633fbb194a9cc932b86e8c7
juberti <justin@fixie.ai>
2024-10-06 20:32:29 -0700

    Add support for gpt-4o-realtime

--- evals/solvers/providers/openai/realtime_solver.py
+++ evals/solvers/providers/openai/realtime_solver.py
@@ -1,12 +1,31 @@
 import asyncio
+import base64
+import io
 import json
 import os
 from typing import Any, Dict, Optional, Union
 
+import pydub
 import websockets
 
 from evals.solvers.solver import Solver, SolverResult
 from evals.task_state import TaskState
+
+
+def _data_url_to_wav(url):
+    if not url.startswith("data:"):
+        raise ValueError("Not a data URL")
+    _, raw_data = url.split(",", 1)
+    return base64.b64decode(raw_data)
+
+
+def _wav_to_24k_pcm(wav_bytes):
+    audio = pydub.AudioSegment.from_file(io.BytesIO(wav_bytes))
+    return audio.set_frame_rate(24000).set_channels(1).set_sample_width(2).raw_data
+
+
+def _pcm_to_base64(pcm_bytes):
+    return base64.b64encode(pcm_bytes).decode("utf-8")
 
 
 class RealtimeSolver(Solver):
@@ -32,12 +51,13 @@ class RealtimeSolver(Solver):
         """
         return self.completion_fn_options["model"]
 
+    @property
     def name(self) -> str:
         return self.model
 
     @property
     def model_version(self) -> Union[str, dict]:
-        return {}
+        return self.model
 
     @property
     def _api_base(self) -> Optional[str]:
@@ -53,27 +73,40 @@ class RealtimeSolver(Solver):
         raw_msgs = [
             {"role": "system", "content": task_state.task_description},
         ] + [msg.to_dict() for msg in task_state.messages]
-        completion_result = asyncio.run(self._foo(raw_msgs))
-        completion_output = completion_result.get_completions()[0]
+        completion_result = asyncio.run(self._ws_completion(raw_msgs))
+        completion_content = completion_result["output"][0]["content"]
+        completion_item = completion_content[0]
+        completion_output = completion_item.get("text") or completion_item.get("transcript")
         solver_result = SolverResult(completion_output, raw_completion_result=completion_result)
         return solver_result
 
-    async def _foo(self, messages):
+    async def _ws_completion(self, messages):
         url = f"{self._api_base}?model={self.model}"
         headers = {"Authorization": f"Bearer {self._api_key}", "OpenAI-Beta": "realtime=v1"}
-        print(url, headers)
         async with websockets.connect(url, extra_headers=headers) as websocket:
-            # session_event = {"type": "session.update", "model": "gpt-4o-realtime-preview-2024-10-01"} ?
-            # await websocket.send(json.dumps(session_event))
-
+            system_message = None
+            modalities = set(["text"])
             for message in messages:
+                message_content_type = type(message["content"])
                 role = message["role"]
                 content = []
-                for item in message["content"]:
-                    if item["type"] == "text":
-                        content.append({"type": "input_text", "text": item["text"]})
-                    elif item["type"] == "audio_url":
-                        content.append({"type": "input_audio", "audio": item["audio_url"]["url"]})
+                if message_content_type is str:
+                    text = message["content"]
+                    if role == "system":
+                        system_message = text
+                    else:
+                        content.append({"type": "input_text", "text": text})
+                        continue
+                elif message_content_type is list:
+                    for item in message["content"]:
+                        if item["type"] == "text":
+                            content.append({"type": "input_text", "text": item["text"]})
+                        elif item["type"] == "audio_url":
+                            wav_bytes = _data_url_to_wav(item["audio_url"]["url"])
+                            pcm_bytes = _wav_to_24k_pcm(wav_bytes)
+                            base64_pcm = _pcm_to_base64(pcm_bytes)
+                            content.append({"type": "input_audio", "audio": base64_pcm})
+                            modalities.add("audio")
                 event = {
                     "type": "conversation.item.create",
                     "item": {"type": "message", "role": role, "content": content},
@@ -82,12 +115,15 @@ class RealtimeSolver(Solver):
             create_response = {
                 "type": "response.create",
                 "response": {
-                    "modalities": ["text"],
+                    "instructions": system_message,
+                    "modalities": list(modalities),
                 },
             }
             await websocket.send(json.dumps(create_response))
+            while True:
+                response_json = await websocket.recv()
+                response = json.loads(response_json)
+                if response["type"] == "response.done":
+                    break
 
-            response = await websocket.recv()
-            print("Received", response)
-            await websocket.close()
-            return response
+        return response["response"]
